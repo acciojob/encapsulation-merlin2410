0b4e352054b756736531217084a470daa8b18912
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2023-03-15 18:32:26 +0530

    Assignment Done First Submisson

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        //r.x = 10;
+        //java: x has private access in com.driver.RWOnly
+        r.setter(10);
+        System.out.println(r.getter());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int x;
+    public void setter(int x)
+    {
+        this.x = x;
+    }
+    public int getter()
+    {
+        return this.x;
+    }
 }
